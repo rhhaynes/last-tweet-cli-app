66af37db4965527e0305b087189b40c495628f7e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-08 18:36:48 +0000

    barebones scraping code

--- lib/todays_tweets/scraper.rb
+++ lib/todays_tweets/scraper.rb
@@ -1,3 +1,10 @@
 class TodaysTweets::Scraper
 
+  def self.scrape(url, how_to_scrape)
+    page = Nokogiri::HTML(open(url))
+    how_to_scrape.collect do |attribute|
+      # code
+    end
+  end
+
 end
