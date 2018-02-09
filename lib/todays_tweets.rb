45688895c3f506b5e5182ac77047a4ccccd0cdd7
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-09 05:01:12 +0000

    moved gem requires to Gemfile

--- lib/todays_tweets.rb
+++ lib/todays_tweets.rb
@@ -1,8 +1,4 @@
 require "todays_tweets/version"
 require "todays_tweets/cli"
 require "todays_tweets/scraper"
-require "todays_tweets/twitter"
-
-require "nokogiri"
-require "open-uri"
-require "colorize"
+require "todays_tweets/twitter_accounts"
