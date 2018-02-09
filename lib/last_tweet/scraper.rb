ef3442c90420b1de5f2c2a1b7cf38fb172d1e11f
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-09 21:10:31 +0000

    added begin/rescue statement to scraper class

--- lib/last_tweet/scraper.rb
+++ lib/last_tweet/scraper.rb
@@ -3,12 +3,16 @@ class LastTweet::Scraper
   def self.scrape(url, what_to_scrape)
     page = Nokogiri::HTML(open(url))
 
-    attributes = what_to_scrape.collect do |k,v|
-      case v[2]
-      when 'hash_key' then [ k, page.css(v[0]).first[ v[1] ]    ]
-      when 'method'   then [ k, page.css(v[0]).first.send(v[1]) ]
-      else                 [ k, page.css(v[0]).first.send(v[1]) ]
+    begin
+      attributes = what_to_scrape.collect do |k,v|
+        case v[2]
+        when 'hash_key' then [ k, page.css(v[0]).first[ v[1] ]    ]
+        when 'method'   then [ k, page.css(v[0]).first.send(v[1]) ]
+        else                 [ k, page.css(v[0]).first.send(v[1]) ]
+        end
       end
+    rescue
+      attributes = what_to_scrape.collect{|k,v| [ k, nil ]}
     end
 
     attributes.to_h
