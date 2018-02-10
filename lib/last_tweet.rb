86db5505aeb5e520d993a1c98cbf2c7bd1a1b615
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-10 21:12:24 +0000

    changed format in last_tweet.rb

--- lib/last_tweet.rb
+++ lib/last_tweet.rb
@@ -1,5 +1,12 @@
-require "last_tweet/version"
-require "last_tweet/cli"
-require "last_tweet/scraper"
-require "last_tweet/tweet"
-require "last_tweet/twitter_account"
+require "colorize"
+require "nokogiri"
+require "open-uri"
+
+require_relative "last_tweet/version"
+require_relative "last_tweet/cli"
+require_relative "last_tweet/scraper"
+require_relative "last_tweet/tweet"
+require_relative "last_tweet/twitter_account"
+
+module LastTweet
+end
