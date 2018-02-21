439a491b56609d6b4b935fc970c67f483da7f815
R. Harris Haynes <harrishaynes@gmail.com>
2018-02-21 11:06:58 -0500

    increased functionality allowing users to specify menu options as well as handles

--- lib/last_tweet/cli.rb
+++ lib/last_tweet/cli.rb
@@ -9,37 +9,38 @@ class LastTweet::CLI
 
   def welcome
     puts
-    puts "Please specify one or more Twitter handles to see their most recent tweet.".colorize(:white)
-    puts "Sample format 1: @twitter_handle".colorize(:white)
-    puts "Sample format 2: @twitter_handle1, @twitter_handle2, etc.".colorize(:white)
+    puts "Please specify one or more Twitter handles to see their most recent tweet."
+    puts "Sample format 1: @twitter_handle"
+    puts "Sample format 2: @twitter_handle1, @twitter_handle2, etc."
     puts
   end
 
   def show_popular
-    puts "If interested the 5 most popular accounts are listed below.".colorize(:white)
-    twitter_accounts = LastTweet::TwitterAccount.most_popular
-    twitter_accounts.each.with_index(1) do |account,i|
+    puts "If interested the 5 most popular accounts are listed below."
+    @twitter_accounts = LastTweet::TwitterAccount.most_popular
+    @twitter_accounts.each.with_index(1) do |account,i|
       puts "#{i}: #{account.name} - #{account.handle}".colorize(:blue)
     end
     puts
   end
 
   def show_menu
-    puts "Whose tweet(s) would you like to see?  Enter exit when finished.".colorize(:white)
-    twitter_handles = 'not_exit'
-    until twitter_handles.downcase == 'exit'
-      twitter_handles = gets.strip
-      unless twitter_handles.downcase == 'exit'
-        show_tweets(twitter_handles)
-        puts "Whose tweet(s) would you like to see next?  Enter exit when finished.".colorize(:white)
+    puts "Whose tweet(s) would you like to see?  Enter exit when finished."
+    @user_input = 'not_exit'
+    until @user_input.downcase == 'exit'
+      @user_input = gets.strip
+      unless @user_input.downcase == 'exit'
+        show_tweets
+        puts "Whose tweet(s) would you like to see next?  Enter exit when finished."
       end
     end
   end
 
-  def show_tweets(twitter_handles)
-    twitter_handles = twitter_handles.gsub(",", " ").split
+  def show_tweets
+    twitter_handles = @user_input.gsub(",", " ").split
     twitter_handles.collect do |handle|
-      tweet = LastTweet::Tweet.create_from_handle(handle)
+      handle = @twitter_accounts[handle.to_i-1].handle if (1..5).include? handle.to_i
+      tweet  = LastTweet::Tweet.create_from_handle(handle)
       if tweet.message == nil
         puts "Last tweet by #{tweet.account.handle} could not be found.  If correct, the account may be private or suspended.".colorize(:red)
         tweet.remove
